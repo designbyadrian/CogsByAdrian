68c5749756d0753d640de8a972f9bbcae11bf9b7
Vault108 <44564111+Vault108@users.noreply.github.com>
2020-12-21 19:11:33 -0500

    Update insult.py

--- insult/insult.py
+++ insult/insult.py
@@ -27,7 +27,7 @@ class Insult(commands.Cog):
             return
 
         async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.get('https://lakerolmaker-insult-generator-v1.p.mashape.com/', params=self.params) as resp:
+            async with session.get('https://evilinsult.com/generate_insult.php?lang=en&type=text', params=self.params) as resp:
 
                 if (resp.status == 200):
 
